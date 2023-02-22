6702ac4e51bba052de00e660f273eed71fdcdffd
StefanNyc2023 <stivmilosavljevic@gmail.com>
2023-02-22 14:24:30 -0500

    Initial commit

---
+++ src/replit89.java
@@ -0,0 +1,12 @@
+public class replit89 {
+
+    public static void main(String[] args) {
+        String str = "Timmy";
+        int len = str.length();
+        System.out.println(len);
+        //this is the way we calculate how many charcteres are present in String
+
+    }
+
+}
+
